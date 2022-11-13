0a27e4cbec2a78b7b2cfac75124bf10eb51b9592
wbrunovieira <wbrunovieira77@gmail.com>
2022-11-13 10:24:36 +0000

    created the screen SignUp

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ import {
 import { THEME } from './src/theme';
 
 import { Loading } from '@components/Loading';
-import { SignIn } from '@screens/SignIn';
+import { SignUp } from '@screens/SignUp';
 
 export default function App() {
 
@@ -28,7 +28,7 @@ export default function App() {
         translucent
       />
 
-      { fontsLoaded ? <SignIn /> : <Loading /> }
+      { fontsLoaded ? <SignUp /> : <Loading /> }
 
       
     </NativeBaseProvider>
