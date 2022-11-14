1d4e85532171a79b50bc66d5b7c1a85559fb9aff
wbrunovieira <wbrunovieira77@gmail.com>
2022-11-14 09:41:57 +0000

    created the app.routes.tsx to private routes using TabBotton

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { useTheme, Box } from "native-base";
 
 import { AuthRoutes } from "./auth.routes";
+import { AppRoutes } from "./app.routes";
 
 export function Routes() {
 
@@ -14,7 +15,7 @@ export function Routes() {
        <Box flex={1} bg="gray.700"
        >
            <NavigationContainer>
-            <AuthRoutes />
+            <AppRoutes />
             </NavigationContainer>
 
 
