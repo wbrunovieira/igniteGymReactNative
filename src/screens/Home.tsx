3f9677ee9e1520552f5abb124983a7025682d40d
wbrunovieira <wbrunovieira77@gmail.com>
2022-11-14 17:45:53 +0000

    groups in FlatList and IsActive when is pressed

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,38 @@
+import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
-import {  VStack } from 'native-base';
-import React from 'react';
+import {  VStack, HStack, FlatList} from 'native-base';
+import React, { useState } from 'react';
 
 export function Home() {
+  const [ groups, SetGroups] = useState(['Ombro', 'Costas', 'Pernas', 'Triceps', 'Biceps'])
+  const [ groupSelected, SetGroupSelected ] = useState('Costas');
+
+ 
+
   return (
     <VStack flex={1}>
     <HomeHeader />
+
+    <FlatList
+      data={groups}
+      keyExtractor={ item => item }
+      renderItem={( { item } ) => (
+        <Group
+          name={item}
+          isActive={groupSelected === item }
+          onPress={() => SetGroupSelected(item)}
+        />
+      )}
+      horizontal
+      showsVerticalScrollIndicator={false}
+      _contentContainerStyle={{px:8}}
+      my={10}
+      maxH={10}
+    />
+
+  
+
+    
   </VStack>
   );
 }
