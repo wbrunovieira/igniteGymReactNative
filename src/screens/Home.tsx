9797147d3c2bde12ebd261d944141e698f8e5a67
wbrunovieira <wbrunovieira77@gmail.com>
2022-11-14 17:52:04 +0000

    added header of list of exercices

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,11 @@
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
-import {  VStack, HStack, FlatList} from 'native-base';
+import {  VStack, HStack, FlatList, Text, Heading} from 'native-base';
 import React, { useState } from 'react';
 
 export function Home() {
   const [ groups, SetGroups] = useState(['Ombro', 'Costas', 'Pernas', 'Triceps', 'Biceps'])
   const [ groupSelected, SetGroupSelected ] = useState('Costas');
-
- 
 
   return (
     <VStack flex={1}>
@@ -29,9 +27,18 @@ export function Home() {
       my={10}
       maxH={10}
     />
+  <VStack flex={1} px={8  }>
+    <HStack justifyContent="space-between" mb={5}>
+      <Heading color="gray.100" fontSize="md">
+          Exercicios
+      </Heading>
+      <Text color="gray.100" fontSize="md">
+        4
+      </Text>
+    </HStack>
 
-  
 
+  </VStack>
     
   </VStack>
   );
