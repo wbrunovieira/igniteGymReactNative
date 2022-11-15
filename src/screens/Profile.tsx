39c4668ec85db76b00b9b530f5a3a77596c54981
wbrunovieira <wbrunovieira77@gmail.com>
2022-11-15 08:15:33 +0000

    started the screen Profile, added the header, photo img with skeleton and button change the photo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,49 @@
-import { Center, Text } from 'native-base';
+import { useState } from 'react';
+
+import { Center, ScrollView, Text, VStack, Skeleton } from 'native-base';
+
+import { ScreenHeader } from '@components/ScreenHeader';
+import { UserPhoto } from '@components/UserPhoto';
+import { TouchableOpacity } from 'react-native';
+
+const PHOTO_SIZE = 33
 
 export function Profile() {
+  const [ photoIsLoading, SetPhotoIsLoading ] = useState(false)
+
   return (
-    <Center flex={1}>
-      <Text color="white">Profile</Text>
-    </Center>
+    <VStack>
+      <ScreenHeader title="Perfil"/>
+
+      <ScrollView>
+        <Center mt={6} px={10}>
+          { photoIsLoading ? <Skeleton
+                                w={PHOTO_SIZE}
+                                h={PHOTO_SIZE}
+                                rounded="full"
+                                startColor="gray.500"
+                                endColor="gray.400"
+                                /> :
+
+          <UserPhoto
+              source={{ uri: 'https://github.com/wbrunovieira.png'}}
+              alt="foto do usuário"
+              size={33}
+          />
+          }
+          <TouchableOpacity>
+            <Text 
+              color="green.500"
+              fontWeight="bold"
+              fontSize="sm"
+              mt={2}
+              mb={8}
+               >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+        </Center>
+      </ScrollView>
+    </VStack>
   );
 }
