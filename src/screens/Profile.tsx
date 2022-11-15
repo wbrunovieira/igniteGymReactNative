98277e2d5bbe04ddc3fb7511ed0189772f4b0d67
wbrunovieira <wbrunovieira77@gmail.com>
2022-11-15 09:22:32 +0000

    finished the screen exercise and add a ScrollView in profile to fix the scroll in iphone

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -14,84 +14,86 @@ export function Profile() {
   const [ photoIsLoading, SetPhotoIsLoading ] = useState(false)
 
   return (
-    <VStack>
-      <ScreenHeader title="Perfil"/>
+    <ScrollView>
+      <VStack>
+        <ScreenHeader title="Perfil"/>
 
-      <ScrollView contentContainerStyle={{ paddingBottom: 36 }} >
-        <Center mt={6} px={10}>
-          { photoIsLoading ? <Skeleton
-                                w={PHOTO_SIZE}
-                                h={PHOTO_SIZE}
-                                rounded="full"
-                                startColor="gray.500"
-                                endColor="gray.400"
-                                /> :
+        <ScrollView contentContainerStyle={{ paddingBottom: 36 }} >
+          <Center mt={6} px={10}>
+            { photoIsLoading ? <Skeleton
+                                  w={PHOTO_SIZE}
+                                  h={PHOTO_SIZE}
+                                  rounded="full"
+                                  startColor="gray.500"
+                                  endColor="gray.400"
+                                  /> :
 
-          <UserPhoto
-              source={{ uri: 'https://github.com/wbrunovieira.png'}}
-              alt="foto do usuário"
-              size={33}
-          />
-          }
+            <UserPhoto
+                source={{ uri: 'https://github.com/wbrunovieira.png'}}
+                alt="foto do usuário"
+                size={33}
+            />
+            }
 
-          <TouchableOpacity>
-            <Text 
-              color="green.500"
-              fontWeight="bold"
-              fontSize="sm"
-              mt={2}
-              mb={8}
-               >
-              Alterar foto
-            </Text>
-          </TouchableOpacity>
-
-          <Input
-            bg="gray.600"
-            placeholder="Nome"
-          />
-          <Input
-            bg="gray.600"
-            placeholder="bruno@email.com"
-            isDisabled
-          />
-        </Center>
-
-        <Center px={10} mt={6} >
-            <Heading
-              color="gray.200"
-              fontSize="md"
-              mb={2}
-              alignSelf="flex-start"
-               mt={6}
-               >
-                Alterar a Senha
-            </Heading>
+            <TouchableOpacity>
+              <Text 
+                color="green.500"
+                fontWeight="bold"
+                fontSize="sm"
+                mt={2}
+                mb={8}
+                >
+                Alterar foto
+              </Text>
+            </TouchableOpacity>
 
             <Input
               bg="gray.600"
-              placeholder="Senha Antiga"
-              secureTextEntry
-            />
-
-            <Input
-              bg="gray.600"
-              placeholder="Nova Senha"
-              secureTextEntry
+              placeholder="Nome"
             />
             <Input
               bg="gray.600"
-              placeholder="Confirme a Nova Senha"
-              secureTextEntry
+              placeholder="bruno@email.com"
+              isDisabled
             />
+          </Center>
 
-            <Button
-              title="Atualizar"
-              mt={4}
-              mb={8}
-            />
-        </Center>
-      </ScrollView>
-    </VStack>
+          <Center px={10} mt={6} >
+              <Heading
+                color="gray.200"
+                fontSize="md"
+                mb={2}
+                alignSelf="flex-start"
+                mt={6}
+                >
+                  Alterar a Senha
+              </Heading>
+
+              <Input
+                bg="gray.600"
+                placeholder="Senha Antiga"
+                secureTextEntry
+              />
+
+              <Input
+                bg="gray.600"
+                placeholder="Nova Senha"
+                secureTextEntry
+              />
+              <Input
+                bg="gray.600"
+                placeholder="Confirme a Nova Senha"
+                secureTextEntry
+              />
+
+              <Button
+                title="Atualizar"
+                mt={4}
+                mb={8}
+              />
+          </Center>
+        </ScrollView>
+      </VStack>
+    </ScrollView>
   );
 }
