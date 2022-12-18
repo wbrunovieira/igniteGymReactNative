af6791f0600fc687424c2ce46c0061eb9dde9c49
wbrunovieira <wbrunovieira77@gmail.com>
2022-12-18 08:17:15 +0000

    added axios and created axios api service, add App Error

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -1,9 +1,12 @@
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from "native-base";
 
 import { useForm, Controller } from 'react-hook-form'
 
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+
+import axios from "axios";
+import { api } from '@services/api'
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
@@ -11,6 +14,9 @@ import BackgroundImg from '@assets/background.png';
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 import { useNavigation } from "@react-navigation/native";
+import { Alert } from "react-native";
+
+import { AppError } from '@utils/AppErrors';
 
 
 type FormDataProps = {
@@ -28,6 +34,7 @@ const signUpSchema = yup.object({
 });
 
 export function SignUp() {
+  const toast = useToast();
 
   const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
     resolver: yupResolver(signUpSchema),
@@ -39,17 +46,24 @@ export function SignUp() {
     navigation.goBack()
   }
 
-  function handleSignUp({ name, email, password, password_confirm}: FormDataProps){
-    fetch('http://192.168.1.103:3333/users',{
-      method: 'POST',
-      headers:{
-        'Accept':'application/json',
-        'Content-Type':'application/json'
-      },
-      body: JSON.stringify({ name, email, password})
-    })
+  async function handleSignUp({ name, email, password }: FormDataProps){
     
-    console.log(name,email, password)
+    try {
+      const response = await api.post('/users', { name, email, password})
+    console.log(response.data)
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+
+      const title = isAppError ? error.message : 'Não foi possível criar a conta. Tente novamente mais tarde';
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    }
+    
+    
   }
 
 
